547aebc78ca740f1b3c33205d95d899ad78f116f
Adriano Volter <91354299+AdrianoVolter@users.noreply.github.com>
2023-04-13 10:34:47 -0300

    feat:adição do faviicon na pagina

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-//import Login from "./components/Login"
-import CadastroNova from "./components/CadastroNova"
+import Login from "./components/Login"
+//import CadastroNova from "./components/CadastroNova"
 function App() {
- 
+
+    // const novaFarmacia = { 
+    
+   
 
   return (
+
     <div className="App">
       
      <h1>Pharmacy Central System</h1>
-      {/* <Login/> */}
-      <CadastroNova/>
+      <Login/>
+      {/* <CadastroNova/> */}
     </div>
      
   )
