d5770a9e7bf561f697047207f65a22506ecccd00
Adriano Volter <91354299+AdrianoVolter@users.noreply.github.com>
2023-04-19 23:30:53 -0300

    feat: criado contexto , finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
 import { Outlet } from "react-router-dom"
 import Header from "./components/Header"
-//import { MedProvider } from "./contexts/MedContext"
+import { MedProvider } from "./contexts/MedContext"
 import { FarmProvider } from "./contexts/FarmContext"
 
 function App() {
 
   return (
-    
-    // <MedProvider>
-    <FarmProvider>
-    <div className="col-12 container-fluid">
-      <Header/>
-      <Outlet />
+    <div className="App">
+      <FarmProvider>
+        <MedProvider>
+          <Header />
+          <Outlet />
+        </MedProvider>
+      </FarmProvider>
     </div>
-    </FarmProvider>
-    // </MedProvider>
+    
    
   )
 }
