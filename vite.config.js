384aecec6a9b42e1f962a830e42fc626075e18ef
adrianovolter <adriano.j.volter@hotmail.com>
2023-08-16 19:32:43 -0300

    adcionando docker file

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
- 
+  server: {
+    host: '0.0.0.0',  // Adicione essa linha - ela fará o Vite servir na interface de rede correta dentro do contêiner Docker
+    port: 5000
+  }
+  
 })
