9eddd6c3a4525e5e1c7df93fc3bbf146c41f2659
adrianovolter <adriano.j.volter@hotmail.com>
2023-09-27 21:47:14 -0300

    integraçao page cadastro

--- src/components/CadastroUser.jsx
+++ src/components/CadastroUser.jsx
@@ -20,12 +20,7 @@ function CadastroUser() {
   const [status, setStatus] = useState("");
   const [dados, setDados] = useState([]);
 
-  useEffect(() => {
-    api.get("/usuarios").then((response) => {
-      setDados(response.data);
-    });
-  }, []);
-
+  
   const handleEnviar = (e) => {
     if (
       nome === "" ||
@@ -47,27 +42,28 @@ function CadastroUser() {
       alert("O CPF deve conter 11 caracteres!");
     } else if (telefone.length < 10) {
       alert("O telefone deve conter 10 caracteres!");
-    } else if (celular.length < 11) {
-      alert("O celular deve conter 11 caracteres!");
     } else {
       e.preventDefault();
       api
-        .post("/usuarios", {
+        .post("/api/usuarios", {
           nome: nome,
           sobrenome: sobrenome,
           genero: genero,
           data_nascimento: data_nascimento,
-          email: email,
-          senha: senha,
           cpf: cpf,
           telefone: telefone,
+          email: email,
+          senha: senha,
+          status: status,
         })
         .then((response) => {
           setDados(response.data);
           alert("Cadastrado com sucesso!");
-          navigate("/login");
+          navigate("/");
+          console.log(response.data);
         })
         .catch((err) => {
+          console.error("ops! ocorreu um erro" + err.message);
           alert("Erro ao cadastrar!");
         }); 
     }
@@ -100,10 +96,62 @@ function CadastroUser() {
             <input
               type="text"
               className="form-control"
-              onChange={handleNome}
+              onChange={e => setNome(e.target.value)}
               name={nome}
               id="nome"
               placeholder="Digite seu nome"
+              required
+            />
+          </div>
+        </div>
+        <div className="col-6">
+          <label className="form-label" htmlFor="nome">
+            Sobrenome:
+          </label>
+          <div className="input-group">
+            <input
+              type="text"
+              className="form-control"
+              onChange={e => setSobrenome(e.target.value)}
+              name={sobrenome}
+              id="sobrenome"
+              placeholder="Digite seu sobrenome"
+              required
+            />
+          </div>
+        </div>
+        <div className="col-6">
+          <label className="form-label" htmlFor="nome">
+            Gênero:
+          </label>
+          <div className="input-group">
+            <select
+              className="form-select"
+              onChange={e => setGenero(e.target.value)}
+              name={genero}
+              id="genero"
+              required
+            >
+              <option value="">Selecione</option>
+              <option value="Feminino">Feminino</option>
+              <option value="Masculino">Masculino</option>
+              <option value="Outro">Outro</option>
+            </select>
+          </div>
+        </div>
+        <div className="col-6">
+          <label className="form-label" htmlFor="nome">
+            Data de Nascimento:
+          </label>
+          <div className="input-group">
+            <InputMask
+              
+              type="date"
+              className="form-control"
+              onChange={e => setData_nascimento(e.target.value)}
+              name={data_nascimento}
+              id="data_nascimento"
+              placeholder="Digite a data de nascimento"
               required
             />
           </div>
@@ -114,10 +162,10 @@ function CadastroUser() {
           </label>
           <div className="input-group">
             <InputMask
-              mask="999.999.999-99"
-              type="text"
+              // mask="999.999.999-99"
+              type="number"
               className="form-control"
-              onChange={handleCpf}
+              onChange={e => setCpf(e.target.value)}
               name={cpf}
               id="cnpj"
               placeholder="Digite seu CPF"
@@ -135,7 +183,7 @@ function CadastroUser() {
             <input
               type="email"
               className="form-control"
-              onChange={handleEmail}
+              onChange={e => setEmail(e.target.value)}
               name={email}
               id="email"
               placeholder="Digite o E-mail"
@@ -152,27 +200,10 @@ function CadastroUser() {
               mask="(99) 9999-9999"
               type="text"
               className="form-control"
-              onChange={handleTelefone}
+              onChange={e => setTelefone(e.target.value)}
               name={telefone}
               id="telefone"
               placeholder="Digite o Telefone"
-            />
-          </div>
-        </div>
-        <div className="col-4">
-          <label className="form-label" htmlFor="nome">
-            Celular:
-          </label>
-          <div className="input-group">
-            <InputMask
-              mask="(99) 99999-9999"
-              type="text"
-              className="form-control"
-              onChange={handleCelular}
-              name={celular}
-              d="celular"
-              placeholder="Digite o Celular"
-              required
             />
           </div>
         </div>
@@ -188,7 +219,7 @@ function CadastroUser() {
               type="password"
               className="form-control"
               minLength={8}
-              onChange={handleSenha}
+              onChange={e => setSenha(e.target.value)}
               name={senha}
               id="senha"
               placeholder="Digite a Senha"
@@ -205,7 +236,7 @@ function CadastroUser() {
               type="password"
               className="form-control"
               minLength={8}
-              onChange={handleConfirmarSenha}
+              onChange={e => setConfirmarSenha(e.target.value)}
               name={confirmarSenha}
               id="confirmar_senha"
               placeholder="Digite a Senha"
@@ -213,6 +244,24 @@ function CadastroUser() {
             />
           </div>
         </div>
+        <div className="col-6">
+          <label className="form-label" htmlFor="nome">
+            Status:
+          </label>
+          <div className="input-group">
+            <select
+              className="form-select"
+              onChange={e => setStatus(e.target.value)}
+              name={status}
+              id="status"
+              required
+            >
+              <option value="">Selecione</option>
+              <option value="Ativo">Ativo</option>
+              <option value="Inativo">Inativo</option>
+            </select>
+          </div>
+          </div>
       </div>
       <div className="row p-3">
         <button
